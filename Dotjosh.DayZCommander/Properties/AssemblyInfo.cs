0a8ea8cf7e7da6a0af1d4d0b4b7220f4803ca19d
Josh Schwartzberg <dotjosh@gmail.com>
2012-07-02 23:09:59 -0400

    Updated for azure deployment

--- Dotjosh.DayZCommander/Properties/AssemblyInfo.cs
+++ Dotjosh.DayZCommander/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dotjosh.DayZCommander")]
+[assembly: AssemblyTitle("DayZCommander")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dotjosh.DayZCommander")]
+[assembly: AssemblyCompany("Dotjosh open source")]
+[assembly: AssemblyProduct("DayZCommander")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

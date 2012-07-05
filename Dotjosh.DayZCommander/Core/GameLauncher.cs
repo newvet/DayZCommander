e0ca70aa01ba0eb6c9d76d3daaece0b07d8890d0
Matthew Krzan <sentientpc@gmail.com>
2012-07-05 16:21:00 -0400

    Refactored GameLauncher and added LocalMachineInfo to begin the process of getting local versions.

--- Dotjosh.DayZCommander/Core/GameLauncher.cs
+++ Dotjosh.DayZCommander/Core/GameLauncher.cs
@@ -1,91 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using Microsoft.Win32;
+﻿using System.Diagnostics;
+using System.Text;
 
 namespace Dotjosh.DayZCommander.Core
 {
-	public class GameLauncher
+	public static class GameLauncher
 	{
-		private static Dictionary<string, string> _armaOARegistryLocations = new Dictionary<string, string>()
-        {
-            { "x86", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Bohemia Interactive Studio\\ArmA 2 OA"},
-            { "x64", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Bohemia Interactive Studio\\ArmA 2 OA" }
-        };
-
-        private static Dictionary<string, string> _armaRegistryLocations = new Dictionary<string, string>()
-        {
-            { "x86", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Bohemia Interactive Studio\\ArmA 2"},
-            { "x64", "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Bohemia Interactive Studio\\ArmA 2" }
-        };
-
-		private static string _steamRegistryLocation = "SOFTWARE\\Valve\\Steam";
-	
-		private static string CpuArchitecture
-		{
-			get
-			{
-				switch(IntPtr.Size)
-				{
-					case 4:
-						return "x86";
-					case 8:
-						return "x64";
-					default:
-						return "x86";
-				}
-			}
-		}
-
-		private static string Arma2Path
-		{
-			get
-			{
-
-				return (string) Registry.GetValue(_armaRegistryLocations[CpuArchitecture], "main", "");
-			}
-		}
-
-		private static string Arma2OAPath
-		{
-			get
-			{
-				return (string) Registry.GetValue(_armaOARegistryLocations[CpuArchitecture], "main", "");
-			}
-		}
-
 		public static void JoinServer(Server server)
 		{
-			var arma2Path = Arma2Path;
-			var arma2OaPath = Arma2OAPath;
-			var arma2OaBetaExePath = Path.Combine(arma2OaPath, @"Expansion\beta\arma2oa.exe");
+			var arguments = new StringBuilder();
 
-			if(string.IsNullOrWhiteSpace(arma2Path))
-			{
-				arma2Path = Path.Combine(new DirectoryInfo(arma2OaPath).Parent.FullName, "ArmA 2");
-			}
-
-			var arguments = @"";
-
-//			var installedWithSteam = arma2OaBetaExePath.IndexOf("steamapps") > 0;
-
-			arguments += " -noSplash -noFilePatching";
-			arguments += " -connect=" + server.IpAddress;
-			arguments += " -port=" + server.Port;
-			arguments += string.Format(" \"-mod={0};expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ\"", arma2Path);
+			arguments.Append(" -noSplash -noFilePatching");
+			arguments.Append(" -connect=" + server.IpAddress);
+			arguments.Append(" -port=" + server.Port);
+			arguments.AppendFormat(" \"-mod={0};expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ\"", LocalMachineInfo.Arma2Path);
 
 			var p = new Process
-			{
-				StartInfo =
-					{
-						FileName = arma2OaBetaExePath,
-						Arguments = arguments,
-						WorkingDirectory = arma2OaPath,
-						UseShellExecute = true,
-					}
-			};
-			p.Start();			
+			        	{
+			        		StartInfo =
+			        			{
+			        				FileName = LocalMachineInfo.Arma2OABetaExe,
+			        				Arguments = arguments.ToString(),
+			        				WorkingDirectory = LocalMachineInfo.Arma2OAPath,
+			        				UseShellExecute = true,
+			        			}
+			        	};
+			arguments.Clear();
+			p.Start();
 		}
 	}
 }
---
+++ Dotjosh.DayZCommander/Core/LocalMachineInfo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Dotjosh.DayZCommander.Core
+{
+	public static class LocalMachineInfo
+	{
+		static LocalMachineInfo()
+		{
+			if(IntPtr.Size == 8)
+			{
+				SetPathsX64();
+			}
+			else
+			{
+				SetPathsX86();
+			}
+			LoadVersions();
+		}
+
+		public static string Arma2Path { get; private set; }
+		public static string Arma2OAPath { get; private set; }
+		public static string Arma2OABetaExe { get; private set; }
+		public static Version Arma2Version { get; private set; }
+		public static Version DayZVersion { get; private set; }
+
+		private static void LoadVersions()
+		{
+		}
+
+		private static void SetPathsX64()
+		{
+			const string arma2Registry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Bohemia Interactive Studio\ArmA 2";
+			const string arma2OARegistry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Bohemia Interactive Studio\ArmA 2 OA";
+
+			SetPaths(arma2Registry, arma2OARegistry);
+		}
+
+		private static void SetPathsX86()
+		{
+			const string arma2Registry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Bohemia Interactive Studio\ArmA 2";
+			const string arma2OARegistry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Bohemia Interactive Studio\ArmA 2 OA";
+
+			SetPaths(arma2Registry, arma2OARegistry);
+		}
+
+		private static void SetPaths(string arma2Registry, string arma2OARegistry)
+		{
+			// Set game paths.
+			Arma2Path = (string)Registry.GetValue(arma2Registry, "main", "");
+			Arma2OAPath = (string)Registry.GetValue(arma2OARegistry, "main", "");
+
+			// If a user does not run a game the path will be null.
+			if(string.IsNullOrWhiteSpace(Arma2Path)
+				&& !string.IsNullOrWhiteSpace(Arma2OAPath))
+			{
+				var pathInfo = new DirectoryInfo(Arma2OAPath);
+				if(pathInfo.Parent != null)
+				{
+					Arma2Path = Path.Combine(pathInfo.Parent.FullName, "arma 2");
+				}
+			}
+			if(!string.IsNullOrWhiteSpace(Arma2Path)
+				&& string.IsNullOrWhiteSpace(Arma2OAPath))
+			{
+				var pathInfo = new DirectoryInfo(Arma2Path);
+				if(pathInfo.Parent != null)
+				{
+					Arma2OAPath = Path.Combine(pathInfo.Parent.FullName, "arma 2 operation arrowhead");
+				}
+			}
+
+			if(string.IsNullOrWhiteSpace(Arma2OAPath))
+			{
+				return;
+			}
+
+			Arma2OABetaExe = Path.Combine(Arma2OAPath, @"Expansion\beta\arma2oa.exe");
+		}
+	}
+}

cdf1704228944bfa47fa384261a23ee8a4778a34
Josh Schwartzberg <dotjosh@gmail.com>
2012-07-06 11:09:57 -0400

    Fixed UI binding and added error handling to clickonce updating

--- Dotjosh.DayZCommander/Core/DayZCommanderUpdater.cs
+++ Dotjosh.DayZCommander/Core/DayZCommanderUpdater.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Deployment.Application;
+using NLog;
 
 namespace Dotjosh.DayZCommander.Core
 {
 	public class DayZCommanderUpdater : BindableBase
 	{
+		private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 		private bool _restartToApplyUpdate;
 
 		public bool RestartToApplyUpdate
@@ -19,30 +22,58 @@ namespace Dotjosh.DayZCommander.Core
 
 		public void StartCheckingForUpdates()
 		{
-			var t = new System.Timers.Timer();
-			t.Interval = TimeSpan.FromHours(2).TotalMilliseconds;
-			t.Elapsed += (sender, args) => CheckForUpdates();
-			CheckForUpdates();
-			t.Start();
+			HandleExceptionsAsWarnings(() =>
+			{
+				if(!ApplicationDeployment.IsNetworkDeployed)
+					return;
+
+				ApplicationDeployment.CurrentDeployment.CheckForUpdateCompleted += CheckForUpdates_Completed;
+				ApplicationDeployment.CurrentDeployment.UpdateCompleted += UpdateCompleted;
+
+				//Start a timer to check for updates every 2 hours
+				var t = new System.Timers.Timer();
+				t.Interval = TimeSpan.FromHours(2).TotalMilliseconds;
+				t.Elapsed += (sender, args) => CheckForUpdates();
+				t.Start();
+
+				//But still check now
+				CheckForUpdates();
+			});
 		}
 
 		private void CheckForUpdates()
 		{
-			 if (ApplicationDeployment.IsNetworkDeployed)
-			 {
-			 	ApplicationDeployment.CurrentDeployment.CheckForUpdateCompleted += (sender, args) =>
-			 	{
-					if(args.UpdateAvailable)
-					{
-						ApplicationDeployment.CurrentDeployment.UpdateCompleted += (o, eventArgs) =>
-						{
-							RestartToApplyUpdate = true;
-						};
-						ApplicationDeployment.CurrentDeployment.UpdateAsync();
-					}
-			 	};
-				ApplicationDeployment.CurrentDeployment.CheckForUpdateAsync();
-			 }
+			HandleExceptionsAsWarnings(() =>
+				ApplicationDeployment.CurrentDeployment.CheckForUpdateAsync()
+			);
+		}
+
+		private void CheckForUpdates_Completed(object sender, CheckForUpdateCompletedEventArgs args)
+		{
+			HandleExceptionsAsWarnings(() =>
+			{
+				if(args.UpdateAvailable)
+				{
+					ApplicationDeployment.CurrentDeployment.UpdateAsync();
+				}
+			});
+		}
+
+		private void UpdateCompleted(object sender, AsyncCompletedEventArgs e)
+		{
+			RestartToApplyUpdate = true;
+		}
+
+		private void HandleExceptionsAsWarnings(Action action)
+		{
+			try
+			{
+				action();
+			}
+			catch(Exception ex)
+			{
+				_logger.Warn(ex);
+			}
 		}
 	}
 }

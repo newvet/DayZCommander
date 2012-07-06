47532dc12f6e1e5b6e735e9255cbf9440811dafd
Josh Schwartzberg <dotjosh@gmail.com>
2012-07-06 02:45:23 -0400

    Added nlog and errors submitting to webservice

--- Dotjosh.DayZCommander/App.xaml.cs
+++ Dotjosh.DayZCommander/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Threading;
 using Caliburn.Micro;
 using Dotjosh.DayZCommander.Core;
+using NLog;
 
 namespace Dotjosh.DayZCommander
 {
@@ -11,7 +14,41 @@ namespace Dotjosh.DayZCommander
 	/// </summary>
 	public partial class App : Application
 	{
+		private static Logger _logger = LogManager.GetCurrentClassLogger();
+
 		public static EventAggregator Events = new EventAggregator();
+		private bool _isUncaughtUiThreadException;
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			AppDomain.CurrentDomain.UnhandledException += UncaughtThreadException;
+			DispatcherUnhandledException += UncaughtUiThreadException;
+			base.OnStartup(e);
+		}
+
+		private void UncaughtUiThreadException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			_isUncaughtUiThreadException = true;
+			_logger.Fatal(e.Exception);
+			var messageBoxResult = MessageBox.Show(
+				"It wasn't your fault, but something went really wrong!\r\nWould you like me to try and restart DayZ Commander for you?",
+				"Oh noes",
+				MessageBoxButton.YesNo);
+			if (messageBoxResult == MessageBoxResult.Yes)
+			{
+				e.Handled = true;
+				System.Windows.Forms.Application.Restart();
+				Current.Shutdown();
+			}
+		}
+
+		private void UncaughtThreadException(object sender, UnhandledExceptionEventArgs e)
+		{
+			if(_isUncaughtUiThreadException)
+				return;
+			var exception = e.ExceptionObject as Exception;
+			_logger.Fatal(exception);
+		}
 	}
 
 	public class PlayersChangedEvent
--- Dotjosh.DayZCommander/Core/GameLauncher.cs
+++ Dotjosh.DayZCommander/Core/GameLauncher.cs
@@ -1,10 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text;
+using NLog;
 
 namespace Dotjosh.DayZCommander.Core
 {
 	public static class GameLauncher
 	{
+		private static Logger _logger = LogManager.GetCurrentClassLogger();
+
 		public static void JoinServer(Server server)
 		{
 			var arguments = new StringBuilder();
@@ -14,18 +18,40 @@ namespace Dotjosh.DayZCommander.Core
 			arguments.Append(" -port=" + server.Port);
 			arguments.AppendFormat(" \"-mod={0};expansion;expansion\\beta;expansion\\beta\\expansion;@DayZ\"", LocalMachineInfo.Arma2Path);
 
-			var p = new Process
-			        	{
-			        		StartInfo =
-			        			{
-			        				FileName = LocalMachineInfo.Arma2OABetaExe,
-			        				Arguments = arguments.ToString(),
-			        				WorkingDirectory = LocalMachineInfo.Arma2OAPath,
-			        				UseShellExecute = true,
-			        			}
-			        	};
-			arguments.Clear();
-			p.Start();
+			try
+			{
+				var p = new Process
+			        		{
+			        			StartInfo =
+			        				{
+			        					FileName = LocalMachineInfo.Arma2OABetaExe,
+			        					Arguments = arguments.ToString(),
+			        					WorkingDirectory = LocalMachineInfo.Arma2OAPath,
+			        					UseShellExecute = true,
+			        				}
+			        		};
+				arguments.Clear();
+				p.Start();
+			}
+			catch(Exception ex)
+			{
+				var joinServerException = new JoinServerException(LocalMachineInfo.Arma2OABetaExe, arguments.ToString(), LocalMachineInfo.Arma2OAPath, ex);
+				_logger.Error(joinServerException);
+			}
+
+		}
+	}
+
+	public class JoinServerException : Exception
+	{
+		public JoinServerException(string fileName, string arguments, string workingDirectory, Exception exception) : base(
+				"There was an error launching the game.\r\n"
+				+ "File Name:" + fileName + "\r\n"
+				+ "Arguments:" + arguments + "\r\n"
+				+ "Working Directory:" + workingDirectory,
+			exception)
+		{
+
 		}
 	}
 }

362c4dbc51b7a0c56fffed69fa11276ed5a7089b
Josh Schwartzberg <dotjosh@gmail.com>
2012-07-22 13:26:18 -0400

    Released .43

--- source/Dotjosh.DayZCommander.App/Properties/AssemblyInfo.cs
+++ source/Dotjosh.DayZCommander.App/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Dotjosh Open Source")]
 [assembly: AssemblyProductAttribute("DayZ Commander")]
 [assembly: AssemblyCopyrightAttribute("Copyright 2012")]
-[assembly: AssemblyVersionAttribute("0.9.0.42")]
-[assembly: AssemblyFileVersionAttribute("0.9.0.42")]
+[assembly: AssemblyVersionAttribute("0.9.0.43")]
+[assembly: AssemblyFileVersionAttribute("0.9.0.43")]
 
